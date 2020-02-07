ccbdb921725f3f09ee92747e793784c072b11cd0
brandonabayer <53543478+brandonabayer@users.noreply.github.com>
2020-02-07 10:48:49 -0600

    Update moviescript.js
    cleaned up moviescript.js to include pseudocode for tmdb ajax request. User onclick event to insert movie variables into function and ajax request

--- moviescript.js
+++ moviescript.js
@@ -1,18 +1,19 @@
 var apiKey = "0a730dda991c741663c9fb1810d54a8c";
 var queryURL = "https://api.themoviedb.org/3/";
 var masterQuery = queryURL + "/api_key=" + apiKey + "&language=en-US" + "&sort_by=primary_release_date.desc&include_adult=" + adultContent + "&include_video=false&page=1&primary_release_date.gte=2020-03-11&primary_release_date.lte=2020-12-31&with_genres=" + movieGenre + "+" + movieThemes;
-var genreArray = "genres";
 //https://developers.themoviedb.org/3/discover/movie-discover
 
-$.ajax({
-    url: queryURL,
-    method: "GET"
-  }).then(function(response){
-    console.log(response);
-})
+// $.ajax({
+//     url: queryURL,
+//     method: "GET"
+//   }).then(function(response){
+//     console.log(response);
+// })
   
 
 // master request
+// onclick #submit will pull movie data and then turn to movie display function
+function getMovie() {
   $.ajax({
     url: masterQuery,
     dataType: "json",
@@ -22,8 +23,8 @@ $.ajax({
       console.log(result);
     }
   });
-  
-  // poster
+}
+  // movie poster will be appended to page as well as movie parameters(Title, Rating, Description, Release date)
   $.ajax({
     url: "https://api.themoviedb.org/3/movie/" + movieid + "images?/api_key=91d8d1139d528d9e3b0afd708d8b9602&language=en-US",
     dataType: "json",
@@ -32,20 +33,20 @@ $.ajax({
       console.log(result)
     }
   })
-  // // title(movieid)
+    // // title(movieid)
     parse(data.)
-  // })
-  // // rating(movieRating)
+
+    // // rating(movieRating)
     parse(data.)
-  // })
-  // // description(movieDescription)
+
+    // // description(movieDescription)
     parse(data.)
-  // })
-  // // release date(movieDate)
+
+    // // release date(movieDate)
     parse(data.)
-  // })
+
   // *************************************************
-  // user parameters to select
+  // user parameters to select regarding what kind of movie they would like to see
 
   // film rating adult content true/false
   if ($('adultContent') > true) {
@@ -53,7 +54,3 @@ $.ajax({
   } else {
     adultContent = "false"
   }
-
-  // movie genre(movieGenre)
-
-
