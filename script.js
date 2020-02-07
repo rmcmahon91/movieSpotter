e5eea0ab75aefe4d330aa8b2c1519b9e4ad5e983
Ryan McMahon <ryan.mcmahon4@gmail.com>
2020-02-06 18:05:45 -0600

    secondmarkup

--- script.js
+++ script.js
@@ -1,16 +1,16 @@
-$("#search").click(function(){
-    var queryUrl = ""
-    var movieName = $("#movieBtn").val()
+// $("#search").click(function(){
+//     var queryUrl = ""
+//     var movieName = $("#movieBtn").val()
 
-    $.ajax({
-        url: queryUrl,
-        method: "GET"
-    })
+//     $.ajax({
+//         url: queryUrl,
+//         method: "GET"
+//     })
     
-    .then(function(respone){
-        
-    })
-})
+//     .then(function(respone){
+    
+//     })
+// })
 
 
 
