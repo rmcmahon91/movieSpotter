208ad3f37daa474295ef2e6640b36a7a763b6a15
Brandon Bayer <brandonabayer@gmail.com>
2020-02-11 14:50:14 -0600

    fetch request

--- moviescript.js
+++ moviescript.js
@@ -1,18 +1,85 @@
 
-// var apiKey = "6e42aba18accf15f6000946c4a42e940";
+var apiKey = "6e42aba18accf15f6000946c4a42e940";
 // var queryURL = "https://api.themoviedb.org/3/movie/" + movieid + "?api_key=6e42aba18accf15f6000946c4a42e940";
 var adultContent = "true";
 var clickedmovieGenre = [];
-var movieThemes = "";
-// var masterQuery = queryURL + "/api_key=" + apiKey + "&language=en-US" + "&sort_by=primary_release_date.desc&include_adult=" + adultContent + "&include_video=false&page=1&primary_release_date.gte=2020-03-11&primary_release_date.lte=2020-12-31&with_genres=" + movieGenre + "," + movieThemes;
-
-// var imgUrl = "https://image.tmdb.org/t/p/original"
-
+// var masterQuery = queryURL + "/api_key=" + apiKey + "&language=en-US" + "&sort_by=primary_release_date.desc&include_adult=" + adultContent + "&include_video=false&page=1&primary_release_date.gte=2020-03-11&primary_release_date.lte=2020-12-31&with_genres=" + clickedmovieGenre + "," + clickedmovieGenre;
+// console.log(response)
+var imgUrl = "https://image.tmdb.org/t/p/w185"
+var movieposterid = [];
 var box = $('input').defaultChecked;
 
+function grabMovies(genreid) {
+  var queryURL = "https://api.themoviedb.org/3/discover/movie?api_key=6e42aba18accf15f6000946c4a42e940&language=en-US"
+    + "&sort_by=popularity.desc&include_adult=false&include_video=false&page=1&primary_release_date.gte=2020-03-11" + "&primary_release_date.lte=2020-12-31&with_genres=" + genreid;
+  $.ajax({
+    method: "GET",
+    url: queryURL,
+    success: function (data) {
+      // console.log(data)
+
+
+      var dataArr = data.results;
+      for (let i = 0; i < dataArr.length; i++) {
+        const element = dataArr[i];
+        // console.log(element)
+        var posterobj = {
+          id: element.id,
+          filepath: element.poster_path,
+          title: element.title,
+          description: element.overview,
+          release: element.release_date,
+        };
+        movieposterid.push(posterobj);
+        // console.log(posterobj)
+      }
+      console.log(movieposterid)
+
+      // results: Array(20)
+      // 0:
+        // adult: false
+        // backdrop_path: "/5BwqwxMEjeFtdknRV792Svo0K1v.jpg"
+        // genre_ids: (5)[12, 18, 9648, 878, 53]
+        // id: 419704
+        // original_language: "en"
+        // original_title: "Ad Astra"
+        // overview: "The near future, a time when both hope and hardships drive humanity to look to the stars and beyond. While a mysterious phenomenon menaces to destroy life on planet Earth, astronaut Roy McBride undertakes a mission across the immensity of space and its many perils to uncover the truth about a lost expedition that decades before boldly faced emptiness and silence in search of the unknown."
+        // popularity: 703.043
+        // poster_path: "/xBHvZcjRiWyobQ9kxBhO6B2dtRI.jpg"
+        // release_date: "2019-09-17"
+        // title: "Ad Astra"
+        // video: false
+        // vote_average: 6
+        // vote_count: 2255
+    
+      // Call the fetch() method to fetch the image, and store it in a variable
+      fetch(imgUrl + movieposterid)
+        .then(res => {
+          if (res.ok) {
+            console.log('SUCCESS')
+          } else {
+            console.log('FAILURE')
+          }
+        })
+        .then(data.movieposterid, console.log(data))
+        .catch(error => console.log('ERROR'))
+
+
+
+      // function will put resolve back inside of the original loop
+      // once that is confirmed 
+      // create div that takes the movie parameters
+      // image div has been created dynamically 
+
+    }
+  })
+}
+
+
+
 $('input').on('click', function () {
-  console.log("box:" + box);
-  console.log("this:" + $(this).prop("checked"));
+  // console.log("box:" + box);
+  // console.log("this:" + $(this).prop("checked"));
 
   //var box = this.defaultChecked;
   //console.log(box)
@@ -22,18 +89,21 @@ $('input').on('click', function () {
     //box = true
 
     var boxvalue = this.value;
-    //var index = clickedmovieGenre.findIndex(boxvalue);
+    // var index = clickedgenreGenre.findIndex(boxvalue);
     //console.log(index)
-       //if (clickedmovieGenre.indexOf(boxvalue) != -1) {
-        clickedmovieGenre.push(boxvalue);
-console.log("index of value " + clickedmovieGenre.indexOf(boxvalue))
-        console.log('44444' + clickedmovieGenre);
-        //console.log(this.value);
-        console.log($(this).attr('id'));
-      }
-  
+    //if (clickedmovieGenre.indexOf(boxvalue) != -1) {
+    clickedmovieGenre.push(boxvalue);
+    // console.log("index of value " + clickedmovieGenre.indexOf(boxvalue))
+    // console.log('44444' + clickedmovieGenre);
+    //console.log(this.value);
+    console.log($(this).attr('id'));
+    var id = $(this).attr('id');
+    // function to pass movies
+    grabMovies(id);
+  }
+
   //in this array if the value is found then remove it
-   else {
+  else {
     //var box = this.defaultChecked;
     //box = false;
     var boxvalue = this.value;
@@ -43,10 +113,11 @@ console.log("index of value " + clickedmovieGenre.indexOf(boxvalue))
     console.log('000000' + clickedmovieGenre);
     //console.log(boxvalue);
 
-  
-}
+
+  }
 
 })
+
   //https://developers.themoviedb.org/3/discover/movie-discover
   // function for search
   // $("#actionBtn").on("click", function (event) {
@@ -84,49 +155,3 @@ console.log("index of value " + clickedmovieGenre.indexOf(boxvalue))
 
 
   // })
-
-
-
-// // master request
-//   $.ajax({
-//     url: masterQuery,
-//     dataType: "json",
-//     method: "GET",
-//     success: function (result) {
-
-//       console.log(result);
-//     }
-//   });
-
-//   // poster
-//   $.ajax({
-//     url: "https://api.themoviedb.org/3/movie/" + movieid + "images?/api_key=91d8d1139d528d9e3b0afd708d8b9602&language=en-US",
-//     dataType: "json",
-//     success: function (result) {
-
-//       console.log(result)
-//     }
-//   })
-//   // // title(movieid)
-//     parse(data.)
-//   // })
-//   // // rating(movieRating)
-//     parse(data.)
-//   // })
-//   // // description(movieDescription)
-//     parse(data.)
-//   // })
-//   // // release date(movieDate)
-//     parse(data.)
-//   // })
-//   // *************************************************
-//   // user parameters to select
-
-//   // film rating adult content true/false
-//   if ($('adultContent') > true) {
-//     adultContent = "true"
-//   } else {
-//     adultContent = "false"
-//   }
-
-//   // movie genre(movieGenre)
